5bf597774bf2322800e90e78139037f529e7f884
rajakishorel <rajakishorelanka@gmail.com>
2017-03-01 23:22:03 +0530

    First Change - Added class members to LoginTest

--- DataDriven_Zoho_Feb17/src/test/java/com/erp/zoho/datadriven/testcases/LoginTest.java
+++ DataDriven_Zoho_Feb17/src/test/java/com/erp/zoho/datadriven/testcases/LoginTest.java
@@ -1,9 +1,14 @@
 package com.erp.zoho.datadriven.testcases;
 
+import org.testng.asserts.SoftAssert;
+
 import com.erp.zoho.datadriven.base.BaseTest;
+import com.erp.zoho.datadriven.util.Xls_Reader;
 
 public class LoginTest extends BaseTest {
 	
-	
+	public String testCaseName = "LoginTest";
+	SoftAssert softAssert = null;
+	Xls_Reader xls;
 
 }
